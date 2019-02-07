b82c77e1b5ef96a79aa4c0a5552c3b4e9943b4a9
Jesse Dietrichson <jesse.dietrichson@xamarin.com>
2019-02-07 14:19:31 -0500

    Add project files.

---
+++ CreateAList/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateAList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>();
+
+            myList.Add(50);
+            myList.Add(50);
+            myList.Add(50);
+            myList.Add(50);
+            myList.Add(50);
+            myList.Add(50);
+            myList.Add(50);
+
+            myList.RemoveAt(1);
+
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+
+            foreach (int j in myList)
+            {
+                Console.WriteLine(j);
+            }
+        }
+    }
+}
